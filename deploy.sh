a24b768f7311daa2b1abfabf3c5e4f1bd5f78629
Aleksey Bykhun <theisease@gmail.com>
2025-04-03 10:51:13 +0800

    deploy

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Deploy to Alchemy Subgraphs using a new subgraph name specifically for Sonic
 graph deploy sonic-chain-erc20-indexer \
-  --version-label v0.1.0 \
+  --version-label v0.1.1 \
   --node https://subgraphs.alchemy.com/api/subgraphs/deploy \
   --deploy-key g5gM7XrReZJ3F \
   --ipfs https://ipfs.satsuma.xyz
